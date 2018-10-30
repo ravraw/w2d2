2205dc3d65dce1251e295e56d61c66db9b0e4187
ravindra rawat <ravinrawat.wd@gmail.com>
2018-10-30 13:07:45 -0600

    server working

--- web_server.js
+++ web_server.js
@@ -4,11 +4,19 @@ const PORT = 1234;
 // a function which handles requests and sends response
 const requestHandler = (request, response) => {
   const { url, method, headers } = request;
-
-  response.end(`Requested Path: ${url}\nRequest Method: ${method}\n`);
+  if (url === '/') response.end('Welcome !!!');
+  else if (url === '/urls')
+    response.end(`www.lighthouselabs.ca \n www.google.com`);
+  else {
+    response.statusCode = 404;
+    response.end('Path not found');
+  }
 };
+
+// initiate server
 const server = http.createServer(requestHandler);
 
+// server listening on posrt 1234
 server.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}.....`);
+  console.log(`Server listening on: http://localhost:${PORT}`);
 });
