7050108c7c3cab9d4e0d2cb52faeb48ae659f402
ravindra rawat <ravinrawat.wd@gmail.com>
2018-10-30 12:36:38 -0600

    http_server working

--- http_server.js
+++ http_server.js
@@ -1,11 +1,37 @@
-const https = require('https');
+const http = require('http');
 
-const server = https.createServer();
+http
+  .createServer((request, response) => {
+    const { headers, method, url } = request;
+    let body = [];
+    request
+      .on('error', err => {
+        console.error(err);
+      })
+      .on('data', chunk => {
+        body.push(chunk);
+      })
+      .on('end', () => {
+        body = Buffer.concat(body).toString();
+        // BEGINNING OF NEW STUFF
 
-server.on('request', (request, response) => {
-  console.log('request recieved');
-});
+        response.on('error', err => {
+          console.error(err);
+        });
 
-server.listen(1234, () => {
-  console.log('Server listening on 1234 ...');
-});
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'application/json');
+        // Note: the 2 lines above could be replaced with this next one:
+        // response.writeHead(200, {'Content-Type': 'application/json'})
+
+        const responseBody = { headers, method, url, body };
+
+        response.write(JSON.stringify(responseBody));
+        response.end();
+        // Note: the 2 lines above could be replaced with this next one:
+        // response.end(JSON.stringify(responseBody))
+
+        // END OF NEW STUFF
+      });
+  })
+  .listen(8080);
